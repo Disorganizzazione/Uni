ef08c5d08a9d12ca39824255b50475fbc91c8b63
Kappanneo <claudio_pannacci@libero.it>
2015-10-22 14:32:41 +0200

    Aggiunta base per programmi in-out.

---
+++ base.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+  char n[4];
+  printf("...Programma avviato: 'quit' per terminare.\n");
+  do
+    {
+      //Inserire funzione qui.
+    }
+  while(scanf("%s", n), n[0]!='q', n[1]!='u', n[2]!='i',n[3]!='t');
+  printf("...Programma terminato.\n");
+  return 0;
+}
