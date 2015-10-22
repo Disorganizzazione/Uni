de758c478ebc2078615482de52ce067bd3ed3dbd
Kappanneo <claudio_pannacci@libero.it>
2015-10-22 12:09:36 +0200

    Abbiamo fatto una calcolatrice!

---
+++ calc.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+float risultato;
+float num;
+char operatore;
+
+do {
+	scanf("%f%c", &num, &operatore);
+	}
+while (operatore==);
+	scanf("%f%c", &num, &operatore);
+
+	
+}
