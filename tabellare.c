b0246c60b97bb012bea46822fc843b8deccf8d3f
Kappanneo <claudio_pannacci@libero.it>
2015-10-22 17:13:26 +0200

    Inizio progetto.

---
+++ tabellare.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+  char n[4];
+  printf("...Programma avviato: 'quit' per terminare.\n");
+  do
+    {
+      printf("Numero varabili?");
+      int variabili;
+      scanf("%d",variabili);
+      for(char variabile= 'A'; variabile - 64 <= variabili, variabile++)
+	printf("%c", variabile);
+    }
+  while(scanf("%s", n), n[0]!='q', n[1]!='u', n[2]!='i',n[3]!='t');
+  printf("...Programma terminato.\n");
+  return 0;
+}
+#include <stdio.h>
+
+int main()
+{
+  
+}
